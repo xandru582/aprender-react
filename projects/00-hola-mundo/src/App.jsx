923f419e773310f5720a9964efacfada3081c2a2
Alejandro Balan Gutierrez <xandru582@gmail.com>
2023-11-19 20:00:04 +0100

    Dia 1

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,21 @@
+export function App() {
+    return(
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+                <img className="tw-followCard-avatar" alt="Avatar random" src="https://unavatar.io/kikobeats">
+                
+                </img>
+                <div>
+                    <strong>Alejandro Balán</strong>
+                    <span>@abalan</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+        
+    )
+}
---
+++ projects/01-citas-react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Hola mundo</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
